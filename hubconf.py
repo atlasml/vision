d1a1ed40ba9fee79e9caf94fe54b1170a48d0732
Ross Taylor <rj-taylor@live.co.uk>
2019-06-18 12:32:41 +0100

    Update hubconf.py

--- hubconf.py
+++ hubconf.py
@@ -1,5 +1,5 @@
 # Optional list of dependencies required by the package
-dependencies = ['torch']
+dependencies = ['torch', 'scipy', 'torchvision']
 
 from torchvision.models.alexnet import alexnet
 from torchvision.models.densenet import densenet121, densenet169, densenet201, densenet161
@@ -12,3 +12,29 @@ from torchvision.models.segmentation import fcn_resnet101, deeplabv3_resnet101
 from torchvision.models.googlenet import googlenet
 from torchvision.models.shufflenetv2 import shufflenet_v2_x0_5, shufflenet_v2_x1_0
 from torchvision.models.mobilenet import mobilenet_v2
+
+
+from sotabench.image_classification import imagenet
+import torchvision.transforms as transforms
+import PIL
+
+
+def benchmark():
+
+    normalize = transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+    input_transform = transforms.Compose([
+        transforms.Resize(256, PIL.Image.BICUBIC),
+        transforms.CenterCrop(224),
+        transforms.ToTensor(),
+        normalize,
+    ])
+
+    imagenet.benchmark(
+        model=resnext101_32x8d(retrained=True),
+        paper_model_name='ResNext-101',
+        paper_arxiv_id='1611.05431',
+        paper_pwc_id='aggregated-residual-transformations-for-deep',
+        input_transform=input_transform,
+        batch_size=256,
+        num_gpu=1
+    )
